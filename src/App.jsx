c760efe852c102bf9f7f6898032e4d592d3bcc42
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-11-15 19:10:44 -0500

    fixed the content

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { FaSearch, FaSlidersH } from "react-icons/fa";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Card from "./components/Card";
+import Yogurt from "./assets/yogurt.jpeg";
+import Pancake from "./assets/pancake.jpeg";
 
-function Navbar() {
+function App() {
   return (
-    <div className="fixed top-4 left-1/2 transform -translate-x-1/2 w-11/12 max-w-md bg-white rounded-full shadow-lg flex items-center p-2 z-50">
-      {/* Search Input */}
-      <div className="flex items-center flex-grow bg-gray-100 rounded-full px-4 py-2">
-        <input
-          type="text"
-          placeholder="Search your food"
-          className="bg-transparent outline-none w-full text-gray-700 placeholder-gray-400"
-        />
-        <FaSearch className="text-gray-500 ml-2" />
-      </div>
+    <div className="flex items-center justify-center min-h-screen flex-col">
+      <div className="w-full max-w-screen-lg md:w-11/12 lg:w-3/4 xl:w-2/3 px-4">
+        <Navbar />
 
-      {/* Filter Icon */}
-      <button className="ml-3 bg-gray-100 rounded-full p-2 text-gray-500 hover:text-black">
-        <FaSlidersH />
-      </button>
+        {/* Responsive grid for cards */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6">
+          <Card img={Yogurt} />
+          <Card img={Pancake} />
+          <Card img={Yogurt} />
+          <Card img={Pancake} />
+          {/* Add more <Card /> components as needed */}
+        </div>
+
+        <Footer />
+      </div>
     </div>
   );
 }
 
-export default Navbar;
+export default App;
