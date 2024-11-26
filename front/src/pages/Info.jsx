c983c99bab2adffcd90ec00ddfd50842b70cf25e
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-11-26 10:37:25 -0500

    fixed the content in info

--- front/src/pages/Info.jsx
+++ front/src/pages/Info.jsx
@@ -7,30 +7,29 @@ const Info = () => {
       <div className="p-8">
         {/* Restaurant Details */}
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold mb-4">
-            Restaurant Information
-          </h2>
+          <h2 className="text-2xl font-semibold mb-4">Sumoki Yakitori - Bar</h2>
           <div className="grid md:grid-cols-2 gap-6">
             {/* Contact Info */}
             <div className="bg-white shadow p-6 rounded-lg">
               <h3 className="text-xl font-bold mb-3">Contact Details</h3>
               <p>
-                <strong>Phone:</strong> (123) 456-7890
+                <strong>Phone:</strong> +1(905)944-9556
               </p>
               <p>
-                <strong>Email:</strong> contact@alwok.com
+                <strong>Email:</strong> sumokicorp@gmail.com
               </p>
               <p>
-                <strong>Address:</strong> 123 Foodie Lane, Culinary City, FL
+                <strong>Address:</strong> 9-18 Fred Varley Drive, Unionville,
+                Ontario L3R 1S3, Canada
               </p>
             </div>
             {/* Opening Hours */}
             <div className="bg-white shadow p-6 rounded-lg">
-              <h3 className="text-xl font-bold mb-3">Opening Hours</h3>
+              <h3 className="text-xl font-bold mb-3">Business Hours</h3>
               <ul>
-                <li>Monday - Friday: 10:00 AM - 9:00 PM</li>
-                <li>Saturday: 11:00 AM - 10:00 PM</li>
-                <li>Sunday: 11:00 AM - 8:00 PM</li>
+                <li>Monday: Closed</li>
+                <li>Tuesday - Friday: 05:00 PM - 11:00 PM</li>
+                <li>Saturday - Sunday: 12:00 PM - 11:00 PM</li>
               </ul>
             </div>
           </div>
@@ -42,7 +41,7 @@ const Info = () => {
           <div className="bg-white shadow p-4 rounded-lg">
             <iframe
               title="Google Maps Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.8354345093623!2d-122.41941608468104!3d37.77492957975965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858064ca39d9fd%3A0xdbff5bfb30f36d4c!2sAlWok!5e0!3m2!1sen!2sus!4v1691186353785!5m2!1sen!2sus"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2876.5567982229386!2d-79.31507412381201!3d43.865014071092794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4d5ca01a1ac55%3A0xa9c427c4a189500!2s18%20Fred%20Varley%20Dr%20Unit%209%2C%20Unionville%2C%20ON%20L3R%201S3%2C%20Canada!5e0!3m2!1sen!2sus!4v1732588281674!5m2!1sen!2sus"
               width="100%"
               height="300"
               style={{ border: 0 }}
