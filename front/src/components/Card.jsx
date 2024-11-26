2100f9547fb3260313d73e080fe44747bf7d8650
freddycmh <50223798+freddycmh@users.noreply.github.com>
2024-11-26 11:34:57 -0500

    made img bigger in card

--- front/src/components/Card.jsx
+++ front/src/components/Card.jsx
@@ -7,7 +7,7 @@ function Card({ img, title, description, price, onClick }) {
       className="w-48 h-75 rounded-lg shadow-lg bg-zinc-900 p-4 flex flex-col items-center relative cursor-pointer "
       onClick={onClick}
     >
-      <div className="rounded-full overflow-hidden bg-gray-100 w-32 h-32 flex items-center justify-center mb-4">
+      <div className="rounded-full overflow-hidden bg-gray-100 w-40 h-40 flex items-center justify-center mb-4">
         <img src={img} alt={title} className="w-full h-full object-cover" />
       </div>
       <div className="text-center mt-auto">
